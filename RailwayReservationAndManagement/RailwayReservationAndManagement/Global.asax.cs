2e3d9d26360b6cf343fde7b9f7ed0d06a328b296
leducthinh2231 <59603110+leducthinh2231@users.noreply.github.com>
2021-11-11 09:51:14 +0700

    Chuc nang Login

--- RailwayReservationAndManagement/RailwayReservationAndManagement/Global.asax.cs
+++ RailwayReservationAndManagement/RailwayReservationAndManagement/Global.asax.cs
@@ -16,6 +16,8 @@ namespace RailwayReservationAndManagement
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
         }
     }
 }
